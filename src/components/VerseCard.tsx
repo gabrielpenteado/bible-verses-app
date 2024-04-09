4b058c1034625ab493efb4b49bc6acb6ad320f13
Gabriel Penteado <gabripenteado@gmail.com>
2024-04-09 01:36:51 -0300

    created a button to download the image

--- src/components/VerseCard.tsx
+++ src/components/VerseCard.tsx
@@ -1,28 +1,42 @@
 import { useVerse } from "@/hooks/useVerse";
-import { Verse } from "@/interfaces/verseInterface";
 import { Skeleton } from "./Skeleton";
+
+import { exportComponentAsJPEG } from "react-component-export-image";
+import React, { useRef } from "react";
+
+import downloadIcon from "./download.svg";
 
 export function VerseCard({ imageURL }: { imageURL: string }) {
   const { data, isFetching } = useVerse();
   //   console.log(imageURL);
-  console.log(data);
+  //   console.log(data);
+  const divRef = useRef(null);
 
   return (
-    <>
-      <img src={imageURL} alt="image" />
-      <div className="absolute top-10 w-full flex justify-center items-center">
-        <div className="flex-col justify-center align-middle">
-          {isFetching && <Skeleton />}
+    <div className="relative">
+      <div ref={divRef}>
+        <img src={imageURL} alt="image" />
+        <div className="absolute top-10 w-full flex justify-center items-center">
+          <div className="flex-col justify-center align-middle">
+            {isFetching && <Skeleton />}
 
-          <h1 className="text-white pb-4 text-sm font-semibold md:text-3xl max-w-[250px] md:max-w-[350px] text-wrap text-left">
-            {data?.text}
-          </h1>
+            <h1 className="text-white pb-4 text-sm font-semibold md:text-3xl max-w-[250px] md:max-w-[350px] text-wrap text-left">
+              {data?.text}
+            </h1>
 
-          <span className="text-white font-semibold md:text-3xl max-w-[250px] flex justify-end pr-4 md:max-w-[350px] ">
-            {data?.book.name} {data?.chapter}:{data?.number}
-          </span>
+            <span className="text-white font-semibold md:text-3xl max-w-[250px] flex justify-end pr-4 md:max-w-[350px] ">
+              {data?.book.name} {data?.chapter}:{data?.number}
+            </span>
+          </div>
         </div>
       </div>
-    </>
+      <button
+        className={`right-2 w-16 bg-black top-[410px] absolute rounded-2xl`}
+        onClick={() => exportComponentAsJPEG(divRef, { fileName: "verse.jpg" })}
+        title="Download"
+      >
+        <img src={downloadIcon} alt="download" />
+      </button>
+    </div>
   );
 }

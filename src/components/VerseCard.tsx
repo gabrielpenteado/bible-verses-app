e7d59dc487bb032bbe57b9491bcd106edc47f41c
Gabriel Penteado <gabripenteado@gmail.com>
2024-04-09 10:18:22 -0300

    fixed layout on lower resolutions

--- src/components/VerseCard.tsx
+++ src/components/VerseCard.tsx
@@ -13,14 +13,14 @@ export function VerseCard({ imageURL }: { imageURL: string }) {
   const divRef = useRef(null);
 
   return (
-    <div className="relative">
+    <div className="relative left-2 ">
       <div ref={divRef}>
         <img src={imageURL} alt="image" />
         <div className="absolute top-10 w-full flex justify-center items-center">
           <div className="flex-col justify-center align-middle">
             {isFetching && <Skeleton />}
 
-            <h1 className="text-white pb-4 text-sm font-semibold md:text-3xl max-w-[250px] md:max-w-[350px] text-wrap text-left">
+            <h1 className="text-white pb-4 text-sm font-semibold md:text-2xl max-w-[250px] md:max-w-[350px] text-wrap text-start">
               {data?.text}
             </h1>
 
@@ -31,7 +31,7 @@ export function VerseCard({ imageURL }: { imageURL: string }) {
         </div>
       </div>
       <button
-        className={`right-2 w-16 bg-black top-[410px] absolute rounded-2xl`}
+        className={` right-2 md:right-2 md:w-16 w-11 bg-black md:top-[410px] top-[235px] absolute rounded-2xl`}
         onClick={() => exportComponentAsJPEG(divRef, { fileName: "verse.jpg" })}
         title="Download"
       >

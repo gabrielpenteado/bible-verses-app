06beb0363832faeb52faef393c1708df3130f1e9
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-12 00:32:34 -0300

    updated logo image path

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,12 @@ import {
 
 import { images } from "./assets/images";
 import { VerseCard } from "./components/VerseCard";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="w-full h-[100vh] justify-center bg-[url('./assets/bible-wallpaper.jpg')] flex-col items-center bg-cover">
-      <img
-        src="../public/logo.png"
-        className="w-[300px] md:w-[500px] pt-4 pb-6 m-auto"
-      />
+      <img src={logo} className="w-[300px] md:w-[500px] pt-4 pb-6 m-auto" />
       <div className="border-[6px] border-double w-fit m-auto flex justify-center items-center align-middle">
         <Carousel>
           <CarouselContent className="md:w-[500px] md:h-[500px] relative w-[300px] h-[300px]">

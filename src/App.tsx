3a4f1f6551ae4b05800df7de1d4fb34b648c8792
Gabriel Penteado <gabripenteado@gmail.com>
2024-11-01 14:56:34 -0300

    update logo.png to bible-api

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     <div className="w-full h-[100vh] justify-center bg-[url('./assets/bible-wallpaper.jpg')] flex-col items-center bg-cover">
       <img
         src={logo}
-        className="w-[300px] md:w-[400px] 2xl:w-[500px] pt-4 pb-6 m-auto"
+        className="w-[300px] md:w-[400px] 2xl:w-[500px] pt-6 pb-6 m-auto"
       />
       <div className="border-[6px] border-double w-fit m-auto flex justify-center items-center align-middle">
         <Carousel>
